c488393f4a2518645bbfc18bcc20f8657d72ff69
allan <allan.nava@ies-italia.it>
2020-05-13 11:45:48 +0200

    update

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('mailgun-upload-template-vscode.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
-
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Mailgun Upload Template!');
 	});
